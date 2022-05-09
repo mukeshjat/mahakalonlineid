8f279225c153d30ae4f5a005bfcdce2e11bbfb7d
Mukesh Jat <mukeshjat3355@gmail.com>
2022-05-09 13:11:15 +0530

    First commit

---
+++ assests/script/script.js
@@ -0,0 +1,27 @@
+$('#myCarousel').carousel({
+    //interval: 3000,
+	interval: false,
+}) 
+
+$('#myCarousel2').carousel({
+    interval: 4000
+})
+
+ 
+ $('.screenShtSec .carousel .carousel-item').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+}); 
